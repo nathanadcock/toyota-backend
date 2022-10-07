62a986ddc544323434d71f3d18aba1a0ec1d5353
Nathan Adcock <nathan@pop-os.localdomain>
2022-10-06 20:43:07 -0500

    added some code to verifySignUp

--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -1,1 +1,20 @@
+const db = require("../models");
+const Employee = db.employees;
 
+checkDuplicateUsernameOrEmail = (req, res, next) => { 
+      // Email
+      Employee.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(user => {
+        if (user) {
+          res.status(400).send({
+            message: "Failed! Email is already in use!"
+          });
+          return;
+        }
+  
+        next();
+      });
+  };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
 
   app.post(
     "/api/auth/signup",
-    verifySignUp.checkDuplicateUsernameOrEmail,
+    verifySignUp.checkDuplicateEmail,
     controller.signup
   );
 
